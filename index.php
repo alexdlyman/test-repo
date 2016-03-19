5c7aa3572d4e0bdfab341bad872ccac672a7e395
Alexander Lyman <me@alexdlyman.com>
2016-03-19 19:19:38 -0400

    initial commi

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>yo dog</title>
+		<script src="scripts.min.js"></script>
+		<link rel="stylesheet" href="style.min.css">
+	</head>
+	<body>
+		<h1>hello all</h1>
+		<p>eat my shorts</p>
+	</body>
+</html>
